b249f1caf549d153594f70694e6a478e2eee2100
Arpan Gayen <arpan.gayen23@gmail.com>
2025-05-06 19:03:28 +0530

    added PROPER movement for Horsey, Rookie Boi, Pope Francis

--- main/Board.java
+++ main/Board.java
@@ -8,7 +8,7 @@ import pieces.*;
 
 public class Board extends JPanel{
     
-    public int tileSize = 85;
+    public int tileSize = 100;
 
     int cols = 8;
     int rows = 8;
@@ -56,6 +56,14 @@ public class Board extends JPanel{
     public boolean isValidMove(Move move){
 
         if(sameTeam(move.piece, move.capture)) {
+            return false;
+        }
+
+        if(!move.piece.isValidMovement(move.newCol, move.newRow)){
+            return false;
+        }
+
+        if(move.piece.MoveCollideswithPiece(move.newCol, move.newRow)){
             return false;
         }
         return true;
@@ -118,6 +126,7 @@ public class Board extends JPanel{
     public void paintComponent(Graphics g){
         Graphics2D g2d = (Graphics2D) g;
 
+        //paint board
         for(int r = 0; r < rows; r++){
             for(int c = 0; c < cols; c++){
                 g2d.setColor((c+r) % 2 ==0? new Color(145,170,192) : new Color(67, 90, 106));
@@ -125,6 +134,21 @@ public class Board extends JPanel{
             }
         }
 
+        //paint highlights
+        if(selectedPiece != null)
+            for(int r = 0; r < rows; r++){
+                for(int c = 0; c < cols; c++){
+                    
+                    if(isValidMove(new Move(this, selectedPiece, c, r))) {
+
+                        g2d.setColor(new Color(68, 180,57,190));
+                        g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
+
+                    }
+                }
+            }
+
+        //paint pieces
         for(Piece piece : pieceList){
             piece.paint(g2d);
         }
--- pieces/Bishop.java
+++ pieces/Bishop.java
@@ -20,4 +20,34 @@ public class Bishop extends Piece{
 
     }
 
+    public boolean isValidMovement(int col, int row){
+        return Math.abs(this.col - col) == Math.abs(this.row - row);
+    }
+
+        public boolean MoveCollideswithPiece(int col, int row){
+
+        //up left
+        if(this.col > col && this.row > row)
+            for(int i = 1; i < Math.abs(this.col - col); i++)
+                if(board.getPiece(this.col - i, this.row - i) != null)
+                    return true;
+        //up right
+        if(this.col < col && this.row > row)
+            for(int i = 1; i < Math.abs(this.col - col); i++)
+                if(board.getPiece(this.col + i, this.row - i) != null)
+                    return true;
+        //down left
+        if(this.col > col && this.row < row)
+            for(int i = 1; i < Math.abs(this.col - col); i++)
+                if(board.getPiece(this.col - i, this.row + i) != null)
+                    return true;
+        //down right
+        if(this.col < col && this.row < row)
+            for(int i = 1; i < Math.abs(this.col - col); i++)
+                if(board.getPiece(this.col + i, this.row + i) != null)
+                    return true;
+
+        return false;
+    }
+
 }
